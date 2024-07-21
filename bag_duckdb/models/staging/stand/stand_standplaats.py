ca4e77d5cf3bc50a6092991a2b11c8256770876c
Juan Jose van der Linden <delostilos@mac.com>
2024-07-22 00:45:10 +0200

    Loading now via arrow instead of pandas

--- bag_duckdb/models/staging/stand/stand_standplaats.py
+++ bag_duckdb/models/staging/stand/stand_standplaats.py
@@ -15,18 +15,25 @@ def model(dbt, session):
     tmpdirname = temp_dir.name
     # unzip bestanden in tempdir    
     ZipFile(f"{sourcedir}{area}{mnemonic}{datum}.zip", 'r').extractall(tmpdirname)
+    print(f"........  {mnemonic} uitgepakt")
     # in actief
-    ZipFile(f"{sourcedir}{area}IA{mnemonic}{datum}.zip", 'r').extractall(tmpdirname)        
+    ZipFile(f"{sourcedir}{area}IA{mnemonic}{datum}.zip", 'r').extractall(tmpdirname)
+    print(f"........  {mnemonic} IA uitgepakt")        
     # niet BAG
     ZipFile(f"{sourcedir}{area}NB{mnemonic}{datum}.zip", 'r').extractall(tmpdirname)
+    print(f"........  {mnemonic} NB uitgepakt") 
     # lijst van te verwerken bestanden
-    xml_files =  glob.glob(f"{tmpdirname}/*{mnemonic}*.xml")
+    xml_files =  sorted(glob.glob(f"{tmpdirname}/*{mnemonic}*.xml"))
+    print(f"........  {len(xml_files)} bestanden te verwerken")
     batches = []
-    for xml_file in xml_files:
-        df = session.sql(f"from st_read('{xml_file}')").df()
-        if not df.empty:
-            rb = pa.record_batch(df)
+    for i, xml_file in enumerate(xml_files, start=1):
+        df = session.sql(f"from st_read('{xml_file}')").arrow()
+        if df.num_rows > 0:
+            rb = df.to_batches()[0]          
             batches.append(rb)
+            print(f"........  Batch {i} toegevoegd")
+        else:
+            print(f"........  Batch {i} overgeslagen, leeg")      
     # temp dir opruimen    
     temp_dir.cleanup()    
     return pa.RecordBatchReader.from_batches(batches[0].schema, batches)
