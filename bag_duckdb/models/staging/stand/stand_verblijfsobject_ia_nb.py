e6f97b15b7b3da3634039e4472615eb50224dfab
Juan Jose van der Linden <delostilos@mac.com>
2024-07-21 16:54:31 +0200

    VBO troubles split VBO IA NB

--- bag_duckdb/models/staging/stand/stand_verblijfsobject_ia_nb.py
+++ bag_duckdb/models/staging/stand/stand_verblijfsobject_ia_nb.py
@@ -3,7 +3,7 @@ import pyarrow as pa
 import tempfile 
 from zipfile import ZipFile
  
-# generiek kopieren voor ieder BAG object
+# generiek kopieren voor ieder BAG object - VBO IA NB apart vanwege run problemen
 def model(dbt, session):
     # ophalen config
     sourcedir = dbt.config.get('sourcedir')
@@ -15,13 +15,13 @@ def model(dbt, session):
     tmpdirname = temp_dir.name
     #print(f"... temp dir {tmpdirname} aangemaakt")
     # unzip bestanden in tempdir    
-    ZipFile(f"{sourcedir}{area}{mnemonic}{datum}.zip", 'r').extractall(tmpdirname)
+    #ZipFile(f"{sourcedir}{area}{mnemonic}{datum}.zip", 'r').extractall(tmpdirname)
     #print('... VBO uitgepakt')
     # in actief
-    #ZipFile(f"{sourcedir}{area}IA{mnemonic}{datum}.zip", 'r').extractall(tmpdirname)  
+    ZipFile(f"{sourcedir}{area}IA{mnemonic}{datum}.zip", 'r').extractall(tmpdirname)  
     #print('... VBO IA uitgepakt')      
     # niet BAG
-    #ZipFile(f"{sourcedir}{area}NB{mnemonic}{datum}.zip", 'r').extractall(tmpdirname)
+    ZipFile(f"{sourcedir}{area}NB{mnemonic}{datum}.zip", 'r').extractall(tmpdirname)
     #print('... VBO NB uitgepakt')
     # lijst van te verwerken bestanden
     xml_files =  sorted(glob.glob(f"{tmpdirname}/*{mnemonic}*.xml"))
